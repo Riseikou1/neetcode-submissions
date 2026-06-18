cf63fbf1b68d0477bdcce4ae1affffb5371050b8
Tmk <temka4705@gmail.com>
2026-06-18 10:57:18 +0900

    Update: surrounded-regions - submission-0

--- Data Structures & Algorithms/surrounded-regions/submission-0.py
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -1,33 +1,49 @@
+class DSU :
+    def __init__(self, n) :
+        self.parent = list(range(n + 1))
+        self.size = [1] * (n + 1)
+    
+    def find(self, node) :
+        if self.parent[node] != node :
+            self.parent[node] = self.find(self.parent[node])
+        return self.parent[node] 
+
+    def union(self, u, v) :
+        pu, pv = self.find(u), self.find(v)
+        if pu == pv : return False
+        if self.size[pu] < self.size[pv] :
+            pu, pv = pv, pu
+        self.size[pu] += self.size[pv]
+        self.parent[pv] = pu
+
+        return True
+
+    def connected(self, u, v) :
+        return self.find(u) == self.find(v)
+
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         rows, cols = len(board), len(board[0])
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        dsu = DSU(rows * cols)
 
-        def dfs(r, c) :
-            if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != 'O' :
-                return 
-
-            board[r][c] = 'T'
-
-            dfs(r, c - 1)
-            dfs(r, c + 1)
-            dfs(r - 1, c)
-            dfs(r + 1, c)
-
-        for r in range(rows) :
-            for c in [0, cols - 1] :
-                if board[r][c] == 'O' :
-                    dfs(r, c)
-
-        for c in range(cols) :
-            for r in [0, rows - 1] :
-                if board[r][c] == 'O' :
-                    dfs(r, c)
-        
         for r in range(rows) :
             for c in range(cols) :
-                if board[r][c] == 'O' :
-                    board[r][c] = 'X'
-                elif board[r][c] == 'T' :
-                    board[r][c] = 'O'
+                if board[r][c] != "O" :
+                    continue
+                
+                if r == 0 or r == rows - 1 or c == 0 or c == cols - 1 :
+                    dsu.union(rows * cols, r * cols + c)
+                else :
+                    for dr, dc in directions :
+                        nr, nc = r + dr, c + dc
+                        if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] == "O" :
+                            dsu.union(r * cols + c, nr * cols + nc)
 
 
+        for r in range(rows) :
+            for c in range(cols) :
+                if dsu.connected(rows * cols, r * cols + c) :
+                    board[r][c] = "O" 
+                else :
+                    board[r][c] = "X"
